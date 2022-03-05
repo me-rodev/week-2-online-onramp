25ef039ea2d7d9e21e2cc3852271191632d7891a
me-rodev <nomaosazeme@gmail.com>
2022-03-05 10:51:41 +0100

    challenge 3 done

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,10 @@
-function increment(arr, value) {}
+function increment(arr, value) {
+    arr.forEach(element => {
+        element.val+=value
+    });
+    return arr
+}
+console.log(increment([{ val: 1 }, { val: 2}, { val: 3 }], 2))
+console.log(increment([{ val: 10 }, { val: 20}, { val: 30 }], 5))
 
 module.exports = increment
