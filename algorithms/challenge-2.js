ff9ecc518c1c23f06a69ff28e16ccac79e47939e
me-rodev <nomaosazeme@gmail.com>
2022-03-05 03:50:07 +0100

    Challenge 2 done

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,18 @@
-function numSwap(number) {}
+function numSwap(number) {
+    //Converting the number into an array
+    let numberArr = String(number).split("").map((number)=>{
+        return Number(number)
+    })
+    let hold = 0
+    for (let i = 0; i < numberArr.length; i+=2) {
+        hold = numberArr[i]
+        numberArr[i] = numberArr[i+1]
+        numberArr[i+1] = hold
+    }
+    return Number(numberArr.join(""))
+}
+
+console.log(numSwap(1234))
+console.log(numSwap(432156))
 
 module.exports = numSwap
