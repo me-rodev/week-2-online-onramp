5cb1673ecc3df6202dc6785374042b58ff72b109
me-rodev <nomaosazeme@gmail.com>
2022-03-06 16:56:26 +0100

    Challenge 1 done

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,27 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+    let checkArr = [0,0,0,0,0,0,0,0,0]
+    let outputArr = []
+    numArr.forEach((element, index) => {
+       checkArr[numArr[index]-1] = element
+    });
+
+    checkArr.forEach((element, index) =>{
+        if (element === 0) {
+            outputArr.push(index + 1)
+        }
+    });
+
+    if (outputArr.length === 0) {
+        return false
+    }else if (outputArr.length === 1) {
+        return outputArr[0]
+    }else {
+        return outputArr
+    }
+    
+}
+console.log(missingNumber([1,2,3,4,6,7,8,9]))
+console.log(missingNumber([1,2,3,4,5,6,8]))
+console.log(missingNumber([1,2,3,4,5,6,7,8,9]))
 
 module.exports = missingNumber
