dff1e98c8e4bd589bcb847a58320496870e8c575
me-rodev <nomaosazeme@gmail.com>
2022-03-06 17:29:51 +0100

    Challenge 4 done

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,14 @@
-function withdraw(amount) {}
+function withdraw(amount) {
+    let less100 = amount % 100
+    let hundred = (amount - less100)/100
+    let less50 = less100 % 50
+    let fifty = (less100 - less50)/50
+    let twenty = less50/20
+
+    return [hundred, fifty, twenty]
+}
+
+console.log(withdraw(250))
+console.log(withdraw(370))
 
 module.exports = withdraw
